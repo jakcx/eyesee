132f59403c1b72d4909ac50dae9ce9aff589d369
jakcx <nolteporta@enayu.com>
2019-10-21 11:44:02 -0400

    Added background substraction

--- livefeed.py
+++ livefeed.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2 as cv
 cap = cv.VideoCapture(0)
+fgbg = cv.createBackgroundSubtractorMOG2()
+
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -12,11 +14,8 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    fgmask = fgbg.apply(frame)
     # Display the resulting frame
-    cv.imshow('frame', gray)
+    cv.imshow('frame',fgmask)
     if cv.waitKey(1) == ord('q'):
         break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
